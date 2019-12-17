59097e123b11b89e86cb2b74a22c1be8be7cef85
AnamKhurshid17 <41837439+AnamKhurshid17@users.noreply.github.com>
2019-12-17 23:08:00 +0500

    booking form adjustment

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -37,14 +37,14 @@ class Home extends Component {
               </div>
               
               </div>
-            <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            {/* <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
               <span className="carousel-control-prev-icon" aria-hidden="true"></span>
               <span className="sr-only">Previous</span>
             </a>
             <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
               <span className="carousel-control-next-icon" aria-hidden="true"></span>
               <span className="sr-only">Next</span>
-            </a>
+            </a> */}
           </div>
 
 <div className="booking_form">
@@ -54,7 +54,8 @@ class Home extends Component {
                                                     alignItems: 'center',
                                                     position: 'absolute',
                                                     width: '100%',
-                                                    top: 60
+                                                    top: 60,
+                                                    left:30
                                                     
                                                   }}>
               <div class="container-fluid" >
